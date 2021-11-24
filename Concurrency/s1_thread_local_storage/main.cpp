8e7d94d04f6888c2018a153a9ef9abbcd482c0c3
duythien <duythienelectric@gmail.com>
2021-11-24 19:08:57 +0700

    thread local storage comment

--- Concurrency/s1_thread_local_storage/main.cpp
+++ Concurrency/s1_thread_local_storage/main.cpp
@@ -2,19 +2,19 @@
 #include <thread>
 #include <atomic>
 
-std::atomic<int> i = 0;
-
+//std::atomic<int> i = 0; // result 123 
+thread_local std::atomic<int> i = 0; // result 111
+// When we change our variable type to trade local, then each thread is going to have its own distinct object.
+// and that's the reason we get 1,1,1 output in this case.
 void foo() {
 	++i;
-	std::cout << "foo :" << i << std::endl;
+	std::cout << i << std::endl;
 }
 
 int main() {
-	//std::cout << "main :" << i << std::endl;
+	
 	std::thread thread_1(foo);
-	
 	std::thread thread_2(foo);
-	
 	std::thread thread_3(foo);
 	
 
