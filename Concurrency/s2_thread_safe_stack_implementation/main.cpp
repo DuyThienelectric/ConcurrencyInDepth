65bcc5cd3c5c3c17757e15c90446622cdbf18b47
duythien <duythienelectric@gmail.com>
2021-12-01 10:42:29 +0700

    thread safe race conditions

--- Concurrency/s2_thread_safe_stack_implementation/main.cpp
+++ Concurrency/s2_thread_safe_stack_implementation/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
-void main() {
-
+#include "s2_thread_safe_stack_implementation.h"
+#include "s2_Trivial_stack.h"
+int main() {
+	return 0;
 }
--- Concurrency/s2_thread_safe_stack_implementation/s2_thread_safe_stack_implementation.h
+++ Concurrency/s2_thread_safe_stack_implementation/s2_thread_safe_stack_implementation.h
@@ -1,37 +1,56 @@
 #pragma once
-
 #include <iostream>
 #include <mutex>
-#include <thread> 
 #include <stack>
+#include <thread>
+#include <memory>
+#include <stdexcept>
 
 template<typename T>
 
-class thread_safe_stack
-{
-	std::stack<T> stk;
+class thead_safe_stack{
+	std::stack<std::shared_ptr<T>> stk;
 	std::mutex m;
 
 public:
-	void push(T element) {
+	void push(T element)
+	{
 		std::lock_guard<std::mutex> lg(m);
-		stk.push(element);
-	}
-	void pop() {
-
-	}
-	T& top() {
-
-	}
-	bool empty() {
-
-	}
-	size_t size() {
-
+		stk.push(std::make_shared<T>(element));
 	}
 
+	std::shared_ptr<T> pop()
+	{
+		std::lock_guard<std::mutex> lg(m);
+		//avoid the race condition between popand empty functions
+		if (stk.empty())
+		{
+			throw std::runtime_error("stack is empty");
+		}
+		// a shared pointer object using the top value of the stack
+		std::shared_ptr<T> res(stk.top());
+		// stack stores the shares pointers instead od elements
+		stk.pop();
+		return res;
+	}
+
+	void pop(T& value)
+	{
+		// contructing the lock gurd object to provide the synchronization
+		std::lock_guard<std::mutex> lg(m);
+		if (stk.empty()) throw std::runtime_error("stack is empty");
+		// get the value out of the stack in this way and assign in to the reference
+		value = *(stk.top().get());
+		stk.pop();
+	}
+	bool empty()
+	{
+		std::lock_guard<std::mutex> lg(m);
+		return stk.empty();
+	}
+	size_t size()
+	{
+		std::lock_guard<std::mutex> lg(m);
+		return stk.size();
+	}
 };
-
-void run() {
-
-}
